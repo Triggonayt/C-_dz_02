07f8c807bb4b26556c2816995677c4b2a08f5450
DontsovD <triggonayt@yandex.ru>
2022-09-21 22:28:32 +0300

    Task 15

--- Program.cs
+++ Program.cs
@@ -12,17 +12,37 @@
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+// Console.Clear();
+// Console.WriteLine("Enter number:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// string num = Convert.ToString(n);
+// if(num.Length > 2)
+// {
+//     Console.WriteLine(num[2]);
+// }
+// else
+// {
+//     Console.WriteLine("not numb3");
+// }
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
 Console.Clear();
 Console.WriteLine("Enter number:");
 int n = Convert.ToInt32(Console.ReadLine());
-string num = Convert.ToString(n);
-if(num.Length > 2)
+int [] array = {1,2,3,4,5,6,7};
+
+if (n == array[5] | n == array[6])
 {
-    Console.WriteLine(num[2]);
+    Console.WriteLine("Weekend");
 }
 else
 {
-    Console.WriteLine("not numb3");
+    Console.WriteLine("Working day");
 }
-
 
